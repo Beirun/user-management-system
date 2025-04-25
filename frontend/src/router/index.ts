d288cce3f906b41e25598924414b55325b2d8bb2
Christina <christinadiamante113@gmail.com>
2025-04-26 02:28:54 +0800

    minor update

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import ForgotPasswordView from '@/views/ForgotPasswordView.vue'
 import RegisterView from '@/views/RegisterView.vue'
+import UserLIstView from '@/views/Admin/UserLIstView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +35,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
+    },
+    {
+      path: '/admin/users',
+      name: 'admin-users',
+      component: UserLIstView,
     }
   ],
 })
