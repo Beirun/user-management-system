09d42da36943ae10e06da10bbdef2415d3eae780
Beirun <orillobernardjay@gmail.com>
2025-04-26 16:40:45 +0800

    update

--- frontend/src/views/Admin/UserLIstView.vue
+++ frontend/src/views/Admin/UserLIstView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import {Table,
+import {
+  Table,
   TableBody,
   TableCaption,
   TableCell,
@@ -40,44 +41,31 @@ onBeforeMount(async() => {
 })
 </script>
 
-<template>  
-    <div class="w-screen h-screen bg-[#15161E] flex justify-center font-sans text-white">
-        <div class="w-1/2 h-1/2 flex flex-col justify-center items-center gap-3">
-            <p class="text-4xl font-extrabold mb-5">Admin User List View</p>
+<template>
+  <div class="w-screen h-screen bg-[#15161E] flex flex-col font-sans text-white gap-5">
+    <NavbarView></NavbarView>
+    <div class="w-full h-full flex justify-center">
+      <div class="w-3/4 flex flex-col gap-3">
+        <p class="text-4xl font-extrabold mb-5">Admin User List View</p>
 
-            <Table>
-                <TableCaption>
-                </TableCaption>
-                <TableHeader>
-                    <TableRow class="hover:bg-transparent">
-                        <TableHead class="w-2/5">
-                            Name
-                        </TableHead>
-                        <TableHead>
-                            Email
-                        </TableHead>
-                        <TableHead class="w-1/5" >
-                            Status
-                        </TableHead>
-
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    
-                    <TableRow class="hover:text-black">
-                        <TableCell>
-                            Title Firstname Lastname
-                        </TableCell>
-                        <TableCell>
-                            Email here
-                        </TableCell>
-                        <TableCell class="w-1/5">
-                            Status here
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </div>
+        <Table>
+          <TableCaption> </TableCaption>
+          <TableHeader>
+            <TableRow class="hover:bg-transparent">
+              <TableHead class="w-2/5"> Name </TableHead>
+              <TableHead> Email </TableHead>
+              <TableHead class="w-1/5"> Status </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow class="hover:text-black">
+              <TableCell> Title Firstname Lastname </TableCell>
+              <TableCell> Email here </TableCell>
+              <TableCell class="w-1/5"> Status here </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
-
+  </div>
 </template>
